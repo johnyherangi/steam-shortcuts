2a9ebab49e5e1e1de8f4d8bdcfd62511d6ec430f
Johny Herangi <johnyherangi@gmail.com>
2024-11-24 18:41:46 +1300

    add form component

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,34 +1,32 @@
+import { Form } from "@/components/ui/form"
 import { createLazyFileRoute } from "@tanstack/react-router"
 import { open } from "@tauri-apps/plugin-dialog"
-import { Form, useForm } from "react-hook-form"
+import { useForm } from "react-hook-form"
 
 export const Route = createLazyFileRoute("/")({
   component: Index,
 })
 
 function Index() {
-  const { control, register, setValue } = useForm()
+  const methods = useForm()
 
   return (
-    <Form
-      onSubmit={({ formData }) => {
-        alert(formData.get("filePath"))
-      }}
-      control={control}
-      className="p-2"
-    >
+    <Form {...methods}>
       <p>Select a shortcuts.vdf file:</p>
       <div className="flex gap-2">
         <button
           className="rounded border px-2 py-1"
           type="button"
           onClick={() => {
-            open().then((filePath) => setValue("filePath", filePath ?? ""))
+            open().then((filePath) => methods.setValue("filePath", filePath ?? ""))
           }}
         >
           Browse
         </button>
-        <input className="w-full rounded border px-2 py-1" {...register("filePath")}></input>
+        <input
+          className="w-full rounded border px-2 py-1"
+          {...methods.register("filePath")}
+        ></input>
       </div>
       <button>Next</button>
     </Form>
